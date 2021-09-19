c30cb204b32e6c1f826b8f194002c071c6571e2d
Riean Esteves <89774815+Rieesteves@users.noreply.github.com>
2021-09-19 20:12:31 +0530

    DAY 4/100

--- src/Pattern.java
+++ src/Pattern.java
@@ -11,15 +11,17 @@ public class Pattern
 
             int rows= sc.nextInt();
 
-            for(int i= rows-1; i>=0;i--)
+            for (int i = rows; i >= 1; i--)
             {
-                for(int j=0; j<=i; j++)
-                {
-                    System.out.print("*"+ " ");
-                }
-                System.out.println();
+            for (int j = 1; j < i; j++)
+            {
+                System.out.print(" ");
             }
-            sc.close();
+            for (int k = i; k <= rows; k++)
+            {
+                System.out.print(k + " ");
+            }
+            System.out.println();
         }
-
+    }
 }
