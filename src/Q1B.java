ce7fc43950f7d669f2a662e8883e9547548d5de0
Riean Esteves <89774815+Rieesteves@users.noreply.github.com>
2021-10-24 21:29:41 +0530

    DAY 41/100

---
+++ src/Q1B.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+class Addition {
+
+    // adding three integer values.
+    public int add(int a, int b, int c)
+    {
+
+        int sum = a + b + c;
+        return sum;
+    }
+
+    // adding three double values.
+    public double add(double a, double b, double c)
+    {
+
+        double sum = a + b + c;
+        return sum;
+    }
+}
+
+class Q1B {
+    public static void main(String[] args)
+    {
+
+        Addition ob = new Addition();
+
+        int sum2 = ob.add(1, 2, 3);
+        System.out.println(
+                "sum of the three integer value :" + sum2);
+        double sum3 = ob.add(1.0, 2.0, 3.0);
+        System.out.println("sum of the three double value :"
+                + sum3);
+    }
+}
