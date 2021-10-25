4a75815907e4d1ed5bdbb4d29cbac3ffad5e35e0
Riean Esteves <89774815+Rieesteves@users.noreply.github.com>
2021-10-25 22:20:14 +0530

    purchase and sales on DAY 41/100

---
+++ src/Purchase.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Purchase extends Sales
+{
+    private int noc;
+    private double amount;
+
+    public void accept() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter no. of copies purchased: ");
+        noc = in.nextInt();
+    }
+
+    public void calculate() {
+        amount = noc * price;
+    }
+
+    public void show() {
+        display();
+        System.out.println("No. of copies: " + noc);
+        System.out.println("Amount: " + amount);
+    }
+
+    public static void main(String args[]) {
+        Purchase obj = new Purchase();
+        obj.input();
+        obj.accept();
+        obj.calculate();
+        obj.show();
+    }
+}
---
+++ src/Sales.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Sales
+{
+    protected String title;
+    protected String author;
+    protected String publication;
+    protected double price;
+
+    public void input() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter book title: ");
+        title = in.nextLine();
+        System.out.print("Enter book author: ");
+        author = in.nextLine();
+        System.out.print("Enter publication name: ");
+        publication = in.nextLine();
+        System.out.print("Enter book price: ");
+        price = in.nextDouble();
+    }
+
+    public void display() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Book Author: " + author);
+        System.out.println("Publication: " + publication);
+        System.out.println("Price: " + price);
+    }
+}
